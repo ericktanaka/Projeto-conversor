fd71d95885bb141e23496fdfc8a000b8b0f6f4d0
ericktanaka <erick.tanaka93@hotmail.com>
2023-01-12 13:10:49 -0300

    Projeto conversor

---
+++ Scripts.js
@@ -0,0 +1,62 @@
+const button = document.getElementById(`convert-button`)
+const select = document.getElementById(`currency-select`)
+
+const dolar = 5.2
+const euro = 5.9
+const bitcoin = 93022.49
+
+const convertValue = () => {
+  const inputValue = document.getElementById(`input-value`).value
+  const realValueText = document.getElementById(`real-value-text`)
+  const currencyValueText = document.getElementById(`currency-value-text`)
+
+  realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputValue)
+
+  if (select.value === `US$ Dólar americano`) {
+    currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputValue / dolar)
+  }
+
+  if (select.value === `€ Euro`) {
+    currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputValue / euro)
+  }
+
+  if (select.value === `₿ Bitcoin`) {
+    currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "BTC",
+    }).format(inputValue / bitcoin)
+  }
+}
+
+const changeCurrency = () => {
+  const currencyName = document.getElementById(`currency-name`)
+  const currencyImg = document.getElementById(`currency-img`)
+
+  if (select.value === `€ Euro`) {
+    currencyName.innerHTML = `Euro`
+    currencyImg.src = "./Assets/Euro.png"
+  }
+
+  if (select.value === `US$ Dólar americano`) {
+    currencyName.innerHTML = `Dólar americano`
+    currencyImg.src = "./Assets/eua.png"
+  }
+
+  if (select.value === `₿ Bitcoin`) {
+    currencyName.innerHTML = `Bitcoin`
+    currencyImg.src = "./Assets/Bitcoin.png"
+  }
+  convertValue()
+}
+
+button.addEventListener(`click`, convertValue)
+select.addEventListener(`change`, changeCurrency)
